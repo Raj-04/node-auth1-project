deaab0de45f1ce1855a623ecc4abd906c4c4124f
Raj-04 <1204patel.raj@gmail.com>
2021-12-13 17:57:27 -0500

    done the logout and all tests passing

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -90,6 +90,25 @@ router.post('/login', checkUsernameExists, (req, res, next) => {
     "message": "no session"
   }
  */
+router.get('/logout', (req, res) => {
+  if(req.session.user) {
+    req.session.destroy(err => {
+      if(err) {
+        res.json({
+          message: 'error logging out'
+        })
+      } else {
+        res.json({
+          message: 'logged out'
+        })
+      }
+    })
+  } else {
+    res.json({
+      message: 'no session'
+    })
+  }
+})
 
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
